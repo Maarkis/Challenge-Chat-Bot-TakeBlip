eb9e7ef0aedc03860fc049b9f504ff98878ab34f
Jean Markis <jeanmarkis85@gmail.com>
2021-07-24 15:54:57 -0300

    Configuration token api github

---
+++ Api/ChallengeTakeBlip/ChallengeTakeBlip/Configuration/GithubConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using ChallengeTakeBlip.Utils;
+
+namespace ChallengeTakeBlip.Configuration
+{
+    public static class GithubConfiguration
+    {
+        public static IServiceCollection ConfigureDependeciesApiGithub(this IServiceCollection services, IConfiguration configuration)
+        {
+            APIGithub github = new();
+            new ConfigureFromConfigurationOptions<APIGithub>(
+                configuration.GetSection("ApiGitHub")).Configure(github);
+            services.AddSingleton(github);
+
+            return services;
+        }
+    }
+}
---
+++ Api/ChallengeTakeBlip/ChallengeTakeBlip/Utils/APIGithub.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChallengeTakeBlip.Utils
+{
+    public class APIGithub
+    {
+        public string Token { get; private set; }
+    }
+}
