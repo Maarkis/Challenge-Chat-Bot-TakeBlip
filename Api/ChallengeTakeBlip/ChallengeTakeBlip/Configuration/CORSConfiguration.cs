5bc97b9f09378cbdda58db33362024c17d7a77ca
Jean Markis <jeanmarkis85@gmail.com>
2021-07-24 15:43:31 -0300

    setup application c#

---
+++ Api/ChallengeTakeBlip/ChallengeTakeBlip/Configuration/CORSConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Configuration
+{
+    public static class CORSConfiguration
+    {
+        public static IServiceCollection ConfigureCORS(this IServiceCollection service)
+        {
+            service.AddCors(options =>
+            {
+                options.AddPolicy("AllowCORS", builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                });
+
+            });
+            return service;
+        }
+    }
+}
